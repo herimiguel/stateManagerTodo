7d11e80e10a6fdb021fe10804c2dc48cefb5306f
Heriberto Miguel <herimiguel84@hotmail.com>
2019-09-11 16:10:29 -0500

    API call with axios

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -19,9 +19,16 @@ const getters = {
     allTodos: state => state.todos
 };
 
-const actions = {};
+const actions = {
+    async fetchTodos({ commit }){
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        commit('setTodos', response.data);
+    }
+};
 
-const mutations = {};
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos)
+};
 
 export default {
     state,
