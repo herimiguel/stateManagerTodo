0728e498c5688abb5c8880bd6991074f05695ece
Heriberto Miguel <herimiguel84@hotmail.com>
2019-09-16 03:17:06 -0500

    delete, update and added css

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
     <div class="container" >
+      <AddTodo />
+      <FilterTodos />
        <Todos />
     </div>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
+import Todos from './components/Todos.vue';
+import AddTodo from './components/AddTodo.vue';
+import FilterTodos from './components/FilterTodos.vue';
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Todos,
+    AddTodo,
+    FilterTodos,
   }
 }
 </script>
---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,33 @@
+<template >
+<div>
+<h2>Filter Todos:</h2>
+<select @change="filterTodos($event)">
+    <option value="200"> 200</option>
+    <option value="200"> 100</option>
+    <option value="200"> 50</option>
+    <option value="200"> 20</option>
+    <option value="200"> 10</option>
+    <option value="200"> 5</option>
+</select>
+
+</div>
+    
+</template>
+<script>
+import {mapActions} from 'vuex';
+
+export default {
+    name: "FilterTodos",
+    methods: mapActions(["filterTodos"]),
+    
+};
+</script>
+
+<style scoped>
+select {
+    margin-top: 20px;
+    padding: 6px;
+    border: #41b883    
+}
+
+</style>
